6444f6d5ebac572e9298d4e2bfb1a05bb3cd52ec
Y39 <y39scode@gmail.com>
2023-03-29 15:57:06 +0900

    setting

---
+++ src/main/java/com/ll/gramgramtest/GramgramtestApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gramgramtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramgramtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GramgramtestApplication.class, args);
+	}
+
+}
